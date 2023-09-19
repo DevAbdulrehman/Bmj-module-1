5b913f5aa91088510119d0f7712bed59b63f4175
DevAbdulrehman <139743505+DevAbdulrehman@users.noreply.github.com>
2023-09-19 12:44:18 +0500

    Add files via upload

---
+++ Java Script/First class/script.js
@@ -0,0 +1,31 @@
+// // Output on console
+// console.clear("I deleted the above messages hahaha");
+// console.log("This is for Message");
+// console.warn("This is for Warning");
+// console.error("This is for Error");
+
+// // Output on DOM
+// document.write(
+//   "<center> <h1> Hi This is message on Dom and it is horizontally centered by center tag and looks bigger because oh h1 tag:> </h1> </center>"
+// );
+// document.write(
+//   "<center> <h2> Hi This is 2nd message on Dom and it is horizontally centered by center tag and looks bigger because of h2 tag:> </h2> </center>"
+// );
+
+// // Output on Windows
+// window.alert("Warning O_O");
+// window.confirm("Are you sure you want to proceed further ?");
+
+// Maths on console
+// console.log ("This is addition" ,"the answer of 14+97=?",14+97) ;
+// console.log ("This is subtraction","the answer of 14-97=?",14-97) ;
+// console.log ("This is multiplication","the answer of 14*97=?",14*97) ;
+// console.log ("This is division","the answer of 14/97=?",14/97) ;
+
+
+// Maths on DOM
+// document.write ("<center> <h1> Maths on Dom </h1> </center>")
+// document.write("This is addition :&nbsp &nbsp the answer of 14+97=? &nbsp", 14+97, "<br> <hr>")
+// document.write("This is subtraction :&nbsp &nbsp" , "the answer of 14-97=? &nbsp", 14-97, "<br> <hr>")
+// document.write("This is multiplication :&nbsp &nbsp" , "the answer of 14*97=? &nbsp", 14*97, "<br> <hr>")
+// document.write("This is division :&nbsp &nbsp" , "the answer of 14/97=? &nbsp", 14/97, "<br> <hr>")
