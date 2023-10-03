ad6f2ac32c41b01e17d97e81a4f37b5d082ac11b
DevAbdulrehman <139743505+DevAbdulrehman@users.noreply.github.com>
2023-10-03 12:46:28 +0500

    Add files via upload

---
+++ Java Script/Fifth class/script.js
@@ -0,0 +1,53 @@
+// // Loops
+
+// let i;
+
+// for(i=1; i<=10; i++){
+//     document.write( `2x${i}=  ${i*2}<br>`)
+// }
+
+// document.write("<center>")
+
+// document.write("<table cellpadding='15'>")
+
+
+// let chose_table= prompt("Write the number of table ")
+
+// let how_much=prompt('how much')
+
+// for(let i=1; i<=how_much; i++){
+//     document.write("<td>")
+//     document.write(`<td> ${chose_table}</td><td>x</td><td>${i}</td><td>=</td> <td>${chose_table*i}</td></tr> <br>`)
+//     document.write("</tr>")
+// }
+
+
+// document.write("</table>")
+
+// document.write("</center>")
+
+
+// By sir bilal
+// let i;
+// let table = prompt("Write the number for table");
+// let how_much=prompt('how much')
+
+// document.write("<center>")
+// document.write("<table border='3' cellpadding='20'>")
+
+// for (i = 1; i <= how_much; i++) {
+//     document.write("<tr>")
+//     document.write(`<td>${table}</td><td>X</td><td>${i}</td><td>=</td><td>${i * table}</td>`)
+//     document.write("</tr>")
+// }
+
+// document.write("</table>")
+// document.write("</center>")
+
+// arrays and for loop
+
+// let stud_name =["Ali","Asad","Abdullah","Ahmed", "Saad"]
+
+// for (let i=0; i<=4; i++){
+//     document.write(`Welcome & ${stud_name[i]} <br>`)
+// }
