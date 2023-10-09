b9e1f9f38874918832a43268569a6ff0c0363f43
DevAbdulrehman <139743505+DevAbdulrehman@users.noreply.github.com>
2023-10-09 12:56:46 +0500

    Add files via upload

---
+++ Java Script/Sixth class/script.js
@@ -0,0 +1,60 @@
+// function Charcode() {
+//     let chose_char=prompt("Write the length of your name ")
+
+
+//     let name_length1= String.fromCharCode (prompt("Give us the acci code of your charecter"));
+//     let name_length2= String.fromCharCode (prompt("Give us the acci code of your charecter"));
+//     let name_length3= String.fromCharCode (prompt("Give us the acci code of your charecter"));
+//     let name_length4= String.fromCharCode (prompt("Give us the acci code of your charecter"));
+//     let name_length5= String.fromCharCode (prompt("Give us the acci code of your charecter"));
+    
+
+//     console.log(name_length1+name_length2+name_length3+name_length4+name_length5)
+    
+
+// }
+
+
+// function trim(){
+     
+//     let sentence=prompt("Write your sentence")
+
+//     let trimmed=sentence.trim()
+
+//     console.log("The lentgh of the sentence u wrote is",sentence.length)
+//     console.log("We trimmed your sentence without removing any words and and now the length is",trimmed.length)
+
+// }
+
+// function includes(){
+
+// }
+
+// Split
+
+// concat
+
+
+// Slice
+// 1 function
+
+function slice_method() {
+    let user=prompt("Write your message")
+
+    let result=user.slice(0,20)
+
+    document.write(result)
+
+    if(result==user){
+        let result=user
+
+        document.write()
+    }
+
+}
+
+ 
+//  length
+
+//  if,else if,else
+
